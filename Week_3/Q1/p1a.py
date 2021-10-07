c191d6f3157766047ac7dca10cd61afc2cd3942e
akanksha <more.akanksha2000@gmail.com>
2021-10-07 14:21:36 +0530

    p1 modified

--- Week_3/Q1/p1a.py
+++ Week_3/Q1/p1a.py
@@ -7,15 +7,15 @@ try:
     records = dns.resolver.resolve(domain,'MX')
 
     print('\nMail exchange servers with their preferences are: \n')
-    print(" ____________________________________________")
-    print("| Prefence  |           MX Record            |")
-    print("|-----------|--------------------------------|")
+    print(" _______________________________________________")
+    print("| Preferennce  |           MX Record            |")
+    print("|--------------|--------------------------------|")
     for record in records:
         r = str(record)
         l = r.split(' ',1)
-        print("| {:<8}  |  {:30}|".format(l[0],l[1]))
+        print("| {:<11}  |  {:30}|".format(l[0],l[1]))
     
-    print("|___________|________________________________|")
+    print("|______________|________________________________|")
 
     print('\n')
 
