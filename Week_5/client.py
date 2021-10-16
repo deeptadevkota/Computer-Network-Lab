2070c6ec6f3468c29c8219bd36b4360216658e65
deeptadevkota <deeptadevkota21@gmail.com>
2021-10-16 12:46:10 +0545

    ssl

--- Week_5/client.py
+++ Week_5/client.py
@@ -1,9 +1,12 @@
 import socket
 import ssl
 
-hostname = 'www.python.org'
-context = ssl.create_default_context()
 
-with socket.create_connection((hostname, 443)) as sock:
+hostname = '127.0.0.1'
+# PROTOCOL_TLS_CLIENT requires valid cert chain and hostname
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.load_verify_locations('cert.pem')
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
         print(ssock.version())
